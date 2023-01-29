b53081ef88fe70f6380823854762d280e8aa0838
VijayN10 <vijaynandurdikar@gmail.com>
2023-01-29 14:50:57 +0530

    Added force vs displacement plot code
    Added plot, currently showing just a vertical line.
    
    Need to modify main fem code

--- FEM_hyperelastic/FEM_hyperelastic.py
+++ FEM_hyperelastic/FEM_hyperelastic.py
@@ -792,6 +792,9 @@ for step in range(0,nsteps):
     err1 = np.sqrt(err1/wnorm)  
     err2 = np.sqrt(err2)/(ndof*nnode)
     print(1,f'Iteration number {nit} Correction {err1} Residual {err2} tolerance {tol}\n')
+    nit =+ 1
+
+  # fprintf(outfile,'\n\n Step %f Load %f\n',step,loadfactor);
 
 
 forcevdisp[1,step] = loadfactor*dloads[2,0]
@@ -800,9 +803,10 @@ forcevdisp[0,step] = w[2]
 
 ##########################
 
-# plt.plot(forcevdisp[0,:],forcevdisp[1,:],'r','LineWidth',3)
-# plt.xlabel({'Displacement'},'FontSize',16)    # Error
-# plt.ylabel({'Force'},'FontSize',16)
+plt.plot(forcevdisp[0], forcevdisp[1], 'r', linewidth=3)
+plt.xlabel('Displacement', fontsize=16)
+plt.ylabel('Force', fontsize=16)
+plt.show()
 
 #########################
 
@@ -811,3 +815,162 @@ scalefactor = 1.0
 for i in range(0,nnode):
   for j in range(0,ndof):
       defcoords[j][i] = coords[j][i] + scalefactor*w[ndof*(i-1)+j-1]  # Check?
+
+
+#########################
+
+# outfile= open("FEM_results.txt","w+")
+
+# def print_results(outfile, nprops,materialprops,ncoord,ndof,nnode,coords,nelem,maxnodes,connect,nelnodes,elident,nfix,fixnodes,ndload,dloads,dofs):
+
+
+
+  # print(outfile,'Nodal Displacements: \n')
+#    if (ndof == 2) 
+#      fprintf(outfile,' Node      Coords         u1       u2 \n');
+#      for i = 1:nnode
+#       fprintf(outfile,'%3d %8.4f %8.4f %8.4f %8.4f\n', ...
+#                                i,coords(1,i),coords(2,i),dofs(2*i-1),dofs(2*i));
+#      end
+#    elseif (ndof == 3) 
+#      fprintf(outfile,' Node            Coords            u1       u2       u3 \n');
+#      for i = 1:nnode
+#       fprintf(outfile,'%3d %8.4f %8.4f %8.4f %8.4f %8.4f %8.4f \n', ...
+#                     i,coords(1,i),coords(2,i),coords(3,i),dofs(3*i-2),dofs(3*i-1),dofs(3*i));
+#      end
+#    end
+
+#    fprintf(outfile,'\n\n Strains and Stresses \n');
+
+
+#    lmncoord = zeros(ncoord,maxnodes);
+#    displacements = zeros(ndof,maxnodes);
+
+# %
+# %   Loop over all the elements
+# %
+#    for lmn = 1:nelem
+
+#     fprintf(outfile,' \n Element; %d ',lmn);
+#     if (ncoord == 2)   
+#     fprintf(outfile,'  \n int pt    Coords          B_11      B_22     B_12      s_11       s_22      s_12 \n');
+
+#     elseif (ncoord == 3) 
+#     fprintf(outfile,'\n int pt         Coords            B_11      B_22     B_33      B_12       B_13      B_23      s_11      s_22      s_33      s_12      s_13      s_23 \n');
+#     end
+# %
+# %   Extract coords of nodes, DOF for the current element
+# %
+#       for a = 1:nelnodes(lmn)
+#         for i = 1:ncoord
+#           lmncoord(i,a) = coords(i,connect(a,lmn));
+#         end
+#         for i = 1:ndof
+#           displacements(i,a) = dofs(ndof*(connect(a,lmn)-1)+i);
+#         end
+#       end
+#       n = nelnodes(lmn);
+#       ident = elident(lmn);
+ 
+#       npoints = numberofintegrationpoints(ncoord,n);
+#       dNdx = zeros(n,ncoord);
+#       dxdxi = zeros(ncoord,ncoord);
+#       xi = zeros(ncoord,1);
+#       x = zeros(ncoord,1);
+# %
+# %  Set up integration points 
+# %
+#       xilist = integrationpoints(ncoord,n,npoints);
+# %
+# %  Loop over the integration points
+# %
+#      for intpt = 1:npoints
+
+# %     Compute shape functions && derivatives wrt local coords
+# %
+#        for i = 1:ncoord
+#          xi(i) = xilist(i,intpt);
+#        end
+#        N = shapefunctions(n,ncoord,ident,xi);      
+#        dNdxi = shapefunctionderivs(n,ncoord,ident,xi);
+# %
+# %     Compute the coords of the integration point
+# %
+#       for i = 1:ncoord
+#         x(i) = 0.;
+#         for a = 1:n
+#           x(i) = x(i) + lmncoord(i,a)*N(a);
+#         end
+#       end
+# %
+# %     Compute the jacobian matrix && its determinant
+# %
+#       for i = 1:ncoord
+#         for j = 1:ncoord
+#           dxdxi(i,j) = 0.;
+#           for a = 1:n
+#             dxdxi(i,j) = dxdxi(i,j) + lmncoord(i,a)*dNdxi(a,j);
+#           end
+#         end
+#       end
+
+#       dxidx = inv(dxdxi);
+# %
+# %     Convert shape function derivatives:derivatives wrt global coords
+# %
+#       for a = 1:n
+#         for i = 1:ncoord
+#           dNdx(a,i) = 0.;
+#           for j = 1:ncoord
+#             dNdx(a,i) = dNdx(a,i) + dNdxi(a,j)*dxidx(j,i);
+#           end
+#         end
+#       end
+# %
+# %     Compute the deformation gradients by differentiating displacements
+# %
+#       for i = 1 : ncoord
+#          for j = 1 : ncoord
+#             F(i,j) = 0.;
+#             if (i==j) F(i,i) = 1.; end
+#             for a = 1 : nelnodes
+#               F(i,j) = F(i,j) ...
+#                          +(displacements(i,a)*dNdx(a,j));
+#             end
+#          end
+#       end
+# %
+# %     Compute Bbar and J
+# %
+#       J = det(F);
+#       B = F*transpose(F);
+# %
+# %     Convert shape function derivatives to derivatives wrt spatial coords
+# %
+#       Finv = inv(F);
+#       for a = 1 : nelnodes
+#         for i = 1 : ncoord
+#           dNdxs(a,i) = 0.;
+#           for j = 1 : ncoord
+#           dNdxs(a,i) = dNdxs(a,i) + dNdx(a,j)*Finv(j,i);  
+#           end
+#         end
+#       end
+# %
+# %     Compute the stress
+# %
+#       stress = Kirchhoffstress(ndof,ncoord,B,J,materialprops);
+#       stress = stress/J;   % Compute Cauchy stress
+      
+#       if (ncoord == 2) 
+
+#       fprintf(outfile,'%5d %7.4f %7.4f %9.4f %9.4f %9.4f %9.4f %9.4f %9.4f \n', ...
+#         intpt,x(1),x(2),B(1,1),B(2,2),B(1,2),stress(1,1),stress(2,2),stress(1,2));
+
+
+#       elseif (ncoord == 3) 
+
+#       fprintf(outfile,'%5d %7.4f %7.4f %7.4f %9.4f %9.4f %9.4f %9.4f %9.4f %9.4f %9.4f %9.4f %9.4f %9.4f %9.4f %9.4f \n',...
+#               intpt,x(1),x(2),x(3), ...
+#               B(1,1),B(2,2),B(3,3),B(1,2),B(1,3),B(2,3), ...
+#               stress(1,1),stress(2,2),stress(3,3),stress(1,2),stress(1,3),stress(2,3));
