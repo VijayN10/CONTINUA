965937e106c301f11c8dc08ab92c85b1d3721f41
VijayN10 <vijaynandurdikar@gmail.com>
2023-02-24 20:37:14 +0530

    Modified  giraffeInputGenerator
    Modified  giraffeInputGenerator to make it compatible for any values of nx and ny
    (here needed for tex2 file)

--- FEM_hyperelastic/FEM_hyperelastic.py
+++ FEM_hyperelastic/FEM_hyperelastic.py
@@ -99,9 +99,9 @@ name = "tex"
 
 def giraffeInputGenerator(rvetype, name, F):
 
-    # # Loop to generate file for each RVE          # Need to check
-                     
+    # Consider rvetype                 
     rr = rvetype  
+
 
     # Create new folder with name of .inp file
     inp = name + '_' + str(rr)                       # inp = tex_0
@@ -127,6 +127,7 @@ def giraffeInputGenerator(rvetype, name, F):
     # F = np.array([[1.2, 0.2],   # Assumption used for testing this function
     #             [0.2, 1.2]])
 
+
     # Get the strain from the deformation gradient
     strain = F - np.eye(2)
 
@@ -141,12 +142,26 @@ def giraffeInputGenerator(rvetype, name, F):
     # print(f'alpha = {alpha}')
 
 
+
     # Initialize disp and calculate the displacement
     # of the ends of the beams
-    disp = np.zeros(((nx + ny) * 2,2))
+    disp = np.zeros(((nx + ny) * 2, 2))
     disp[:,0] = alpha1*X[:,0] + alpha2*X[:,1]
     disp[:,1] = alpha3*X[:,0] + alpha4*X[:,1]
-    # print(f'disp = {disp}')
+
+    
+
+    displacement_block = []
+
+    for i in range(1, ((nx + ny) * 2) + 1):
+        block = ("""\n
+NodalDisplacement {} NodeSet {} CS 125 NTimes 2
+//Time UX UY UZ ROTX ROTY ROTZ
+0\t0\t0\t0\t0\t0\t0
+1\t{}\t{}\t0\t0\t0\t0\n
+""").format(i, i + 4, disp[i-1][0], disp[i-1][1])
+        displacement_block.append(block)
+
 
     # Read the top portion from grfTop{i}.txt
     with open("Giraffe/RVE_data/grfTop" + str(rr)  + ".txt", "r") as top_file:
@@ -156,7 +171,7 @@ def giraffeInputGenerator(rvetype, name, F):
     with open("Giraffe/RVE_data/grfBottom" + str(rr) + ".txt", "r") as bottom_file:
         bottom = bottom_file.read()
 
-
+  
     # Open the file for writing
 
     filepath = 'Giraffe/' + inp + '/' + inp + '.inp'         # filepath - E:/Softwares/01/Giraffe/tex_0/tex_0.inp
@@ -168,79 +183,9 @@ def giraffeInputGenerator(rvetype, name, F):
 
     # Displacement block
 
-    # Left
-
-        file.write("""
-        
-    Displacements 8
-    ///////////////////////////////
-    ///////////left////////////////
-    ///////////////////////////////
-    NodalDisplacement 1 NodeSet 5 CS 125 NTimes 2
-    //Time UX UY UZ ROTX ROTY ROTZ
-    0	0	0	0	0	0	0     
-        """)   
-        file.write(f'2 {disp[0,0]:.12f} {disp[0,1]:.12f} 0 0 0 0\n')
-        file.write("""
-    NodalDisplacement 2 NodeSet 6 CS 125 NTimes 2
-    //Time UX UY UZ ROTX ROTY ROTZ
-    0	0	0	0	0	0	0
-        """)
-        file.write(f'2 {disp[1,0]:.12f} {disp[1,1]:.12f} 0 0 0 0\n\n')
-
-    # Right
-
-        file.write("""
-    ///////////////////////////////
-    ///////////right///////////////
-    ///////////////////////////////
-    NodalDisplacement 3 NodeSet 7 CS 125 NTimes 2
-    //Time UX UY UZ ROTX ROTY ROTZ
-    0	0	0	0	0	0	0
-        """)
-        file.write(f'2 {disp[2,0]:.12f} {disp[2,1]:.12f} 0 0 0 0\n')
-        file.write("""
-    NodalDisplacement 4 NodeSet 8 CS 125 NTimes 2
-    //Time UX UY UZ ROTX ROTY ROTZ
-    0	0	0	0	0	0	0
-        """)
-        file.write(f'2 {disp[3,0]:.12f} {disp[3,1]:.12f} 0 0 0 0\n\n')
-
-    # Bottom
-
-        file.write("""
-    ///////////////////////////////
-    ///////////bottom//////////////
-    ///////////////////////////////
-    NodalDisplacement 5 NodeSet 9 CS 125 NTimes 2
-    //Time UX UY UZ ROTX ROTY ROTZ
-    0	0	0	0	0	0	0
-        """)
-        file.write(f'2 {disp[4,0]:.12f} {disp[4,1]:.12f} 0 0 0 0\n')
-        file.write("""
-    NodalDisplacement 6 NodeSet 10 CS 125 NTimes 2
-    //Time UX UY UZ ROTX ROTY ROTZ
-    0	0	0	0	0	0	0
-        """)
-        file.write(f'2 {disp[5,0]:.12f} {disp[5,1]:.12f} 0 0 0 0\n\n')
-
-    # Top
-
-        file.write("""
-    ///////////////////////////////
-    ///////////top/////////////////
-    ///////////////////////////////
-    NodalDisplacement 7 NodeSet 11 CS 125 NTimes 2
-    //Time UX UY UZ ROTX ROTY ROTZ
-    0	0	0	0	0	0	0
-        """)
-        file.write(f'2 {disp[6,0]:.12f} {disp[6,1]:.12f} 0 0 0 0\n')
-        file.write("""
-    NodalDisplacement 8 NodeSet 12 CS 125 NTimes 2
-    //Time UX UY UZ ROTX ROTY ROTZ
-    0	0	0	0	0	0	0
-        """)
-        file.write(f'2 {disp[7,0]:.12f} {disp[7,1]:.12f} 0 0 0 0\n\n')
+        file.write("\n\nDisplacements {}\n\n".format((nx + ny) * 2))
+        for i in range((nx + ny) * 2):
+            file.write(displacement_block[i])
 
 
     # Bottom part of Giraffe input file
